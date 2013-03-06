ce2d96ebfcf4a572a98c347c1ac74153a28e8113
Jacob Stevenson <js850@cam.ac.uk>
2013-03-06 16:47:20 +0000

    finish with the routine for sampling from low lying minima
    there are probably still bugs

--- playground/nested_sampling/bh_sampling.py
+++ playground/nested_sampling/bh_sampling.py
@@ -5,8 +5,28 @@ improve sampling in "nested sampling" at low energies
 import numpy as np
 from scipy.special import gamma, gammaln
 
+from database_eigenvecs import HessianEigs
+
 from pygmin.systems import LJCluster
 from pygmin.utils.rotations import vec_random_ndim
+from pygmin.mindist import PointGroupOrderCluster
+from pygmin.utils.hessian import sort_eigs, get_eig
+from pygmin.thermodynamics import logproduct_freq2
+
+
+class LJClusterNew(LJCluster):
+    """same as LJCluster, but attach some additional information"""
+    def __init__(self, natoms):
+        super(LJClusterNew, self).__init__(natoms)
+        self.nzero_modes = 6
+        self.k = 3 * natoms - self.nzero_modes
+    
+    def get_metric_tensor(self):
+        return None
+    
+    def get_pgorder(self):
+        raise 
+
 
 def vector_random_uniform_hypersphere(k):
     """return a vector sampled uniformly in a hypersphere of dimension k"""
@@ -16,37 +36,49 @@ def vector_random_uniform_hypersphere(k):
     return p * u
     
 
-def sample_uniformly_in_basin_harmonic(m, Emax):
+def sample_uniformly_in_basin_harmonic(m, Emax, k):
     """assuming the harmonic approximation return a configuration with energy less than Emax sampled uniformly from the basin defined by m
     
     this is exact in the harmonic approximation 
     """
-    dx = np.zeros(m.coords.shape)
-
-    k = None #TODO number of vibrational degrees of freedom = 3*N - 6
-    eigs = m.eval_vecs #TODO
+    nm = m.hessian_eigs[0]
+    freq = nm.eigenvalues
+    vectors = nm.eigenvectors
+    nzero = len(freq) - k
 
     # get uniform random vector in k dimensional hypersphere
     f = vector_random_uniform_hypersphere(k)
-    i = 0
-    for eval, evec in eigs:
+    
+    # scale f according to Emax
+    f *= np.sqrt(2. * (Emax - m.energy)) #TODO check prefactor
+
+    # create the random displacement vector
+    dx = np.zeros(m.coords.shape)
+    for i in range(k):
         if eval > 1e-4:
-            dx += Emax * f[i] * evec / eval #TODO check prefactor
-        i += 1
+            dx += f[i] * vectors[:,i+nzero] / freq[i+nzero]
     
     return m.coords + dx
     
 
-def sample_uniformly_in_basin(m, Emax):
+def sample_uniformly_in_basin(m, Emax, potential, k):
     """return a configuration with energy less than Emax sampled uniformly from the basin defined by m
     """
     # displace randomly from the minimum according to the eigenvalues and eigenvectors
+    E = Emax + 1.
+    count = 0
+    while E > Emax:
+        coords = sample_uniformly_in_basin_harmonic(m, Emax, k)
+        E = potential.getEnergy(coords)
+        print "created structure with energy", E, "Emax", Emax, "Emin", m.energy, count
+        count += 1
+        
     
     # now do a short monte carlo sampling to improve unbiased sampling
     
-    raise NotImplementedError
+    return coords, E
 
-def compute_log_phase_space_volume(m, Emax):
+def compute_log_phase_space_volume(m, Emax, k):
     """return the log (base e) of the phase space volume of minima m up to energy Emax
     
     V = Integral from m.energy to Emax of the harmonic density of states DoS(E)
@@ -60,58 +92,128 @@ def compute_log_phase_space_volume(m, Emax):
 
         V = (Emax - m.energy)**(k+1) / ((k+1) * np.gamma(k) * prod_freq * O_k)
     """
-    # I probably want to do this in log space
     from numpy import log
-    k = None
-    prod_freq = None
-    O_k = None
-    logV = (k+1) * log(Emax - m.energy) - log(k+1) - gammaln(k) - log(prod_freq) - log(O_k)
-    raise NotImplementedError
+    if m.energy > Emax:
+        raise ValueError("Emax (%g) must be greater than the energy of the minimum (%g)" % (Emax, m.energy))
+    logV = (k+1) * log(Emax - m.energy) - log(k+1) - gammaln(k) - m.fvib - log(m.pgorder)
     return logV
 
-def select_uniformly_weighted(weights):
+def weighted_pick(weights):
     """sample uniformly from the objects with given weights
     
     return the selected index
     """
-    raise NotImplementedError
+    if len(weights) == 0:
+        raise ValueError("weights must not have zero length")
+    r = np.random.uniform(0., sum(weights))
+    s = 0.0
+#    print r, len(weights)
+    for i in range(len(weights)):
+        s += weights[i]
+        if r < s: return i
+    return len(weights) - 1
 
-def sample_from_database(db, Emax):
+def sample_from_database(system, db, Emax):
     # calculate the harmonic phase space volume of each minima and store it in list `weights`
     weights = []
-    minima = db.minima()
-    for m in minima:
-        lV = compute_log_phase_space_volume(m, Emax)
-        weights.append(np.exp(lV))
+    minima = []
+    for m in db.minima():
+        if m.energy < Emax:
+            lV = compute_log_phase_space_volume(m, Emax, system.k)
+            weights.append(np.exp(lV))
+            minima.append(m)
     
-    # select minima uniformly given `weights`
-    index = select_uniformly_weighted(weights)
+    # select a minimum uniformly given `weights`
+    index = weighted_pick(weights)
+#    print index, len(weights), len(minima)
     m = minima[index]
     
     # sample configuration uniformly from the basin of minima m 
-    sample_uniformly_in_basin(m, Emax)
-    pass
+    coords, E = sample_uniformly_in_basin(m, Emax, system.get_potential(), system.k)
+
+    return coords, E
+
+def get_normalmodes(hessian, metric=None, eps=1e-4):
+    ''' calculate and return normal mode frequencies and vectors
     
+    Parameters
+    ----------
+    hessian:
+        hessian marix
+        
+    metric: 
+        mass weighted metric tensor
+    '''
+    A = hessian
+    if metric is not None:
+        A = np.dot(np.linalg.pinv(metric), hessian)
+   
+    freq, evecs = np.linalg.eig(A)
+    
+    
+    if(np.max(np.abs(np.imag(freq))) > eps):
+        print freq
+        raise ValueError("imaginary eigenvalue in frequency calculation"
+                         ", check hessian + metric tensor\nthe largest imaginary part is %g"%np.max(np.abs(np.imag(freq))))
+    
+    freq = np.real(freq)
+    freq, evecs = sort_eigs(freq, evecs)
+    return freq, evecs
+
+ 
 def generate_database(natoms=31):
     """return a database with all important low energy minima and all information
     necessary to compute the density of states
     """
     # define the system
-    system = LJCluster(natoms)
+    system = LJClusterNew(natoms)
     
     # use basinhopping to find the low energy minima and store them in a database
     db = system.create_database("lj31.db")
     bh = system.get_basinhopping(database=db)
-    nsteps = 1000
+    nsteps = 1
     bh.run(nsteps)
     
     # get the point group information
+    print "getting the point group information"
+    determine_pgorder = PointGroupOrderCluster(system.get_compare_exact())
+    for m in db.minima():
+        m.pgorder = determine_pgorder(m.coords)
+#        print m.pgorder
+    db.session.commit()
 
     # get the frequencies
+    print "getting the normal mode frequencies"
+    for m in db.minima():
+        # calculate the Hessian
+        pot = system.get_potential()
+        e, g, hess = pot.getEnergyGradientHessian(m.coords)
+        
+        # calculate the normal modes from the hessian
+        freq, evec = get_normalmodes(hess, metric=system.get_metric_tensor())
+        # calculate the log product of the positive normal mode frequencies
+        n, lnf = logproduct_freq2(freq, system.nzero_modes)
+        m.fvib = lnf
+        
+        # calculate the eigenvalues and eigenvectors of the hessian and attach them to the database
+        eval, evec = get_eig(hess)
+        if len(m.hessian_eigs) == 0: 
+            nm = HessianEigs(m, eval, evec)
+
+    db.session.commit()
+    
     
     # combine the point group information and frequencies to compute the density of states
     
-    return db
+    return system, db
     
 if __name__ == "__main__":
-    generate_database()
+    system, db = generate_database()
+    
+    Emin = db.minima()[0].energy
+    Emax = Emin + 5.
+
+    k = system.k
+    for i in range(10):
+        coords, E = sample_from_database(system, db, Emax)
+    
--- playground/nested_sampling/database_eigenvecs.py
+++ playground/nested_sampling/database_eigenvecs.py
@@ -23,20 +23,58 @@ class NormalModes(Base):
     vectors : 2-d numpy array
         vectors[:,i] is the eigenvector associated to the normal mode frequency freq[i].
         the eigenvectors should be orthogonal and normalized
+    nzero : integer, optional
+        the number of zero frequencies (from global symmetries).  Default 0
+    nnegative : integer, optional
+        the number of negative frequencies (e.g. at a saddle point).  Default 0
     """
     __tablename__ = "tbl_normal_modes"
 
     freq = deferred(Column(PickleType))
     vectors = deferred(Column(PickleType))
+    nzero = Column(Integer)
+    nnegative = Column(Integer)
 
     _minimum_id = Column(Integer, ForeignKey('tbl_minima._id'), primary_key=True)
     minimum = relationship("Minimum",
                             primaryjoin="Minimum._id==NormalModes._minimum_id",
                             backref='normal_modes', uselist=False)
-    def __init__(self, m, freq, vectors):
+    def __init__(self, m, freq, vectors, nzero=0, nnegative=0):
         self.minimum = m
         self.freq = freq
         self.vectors = np.copy(vectors)
+        self.nzero = nzero
+        self.nnegative = nnegative
+
+class HessianEigs(Base):
+    """hessian eigenvalues and associated eigenvectors
+    
+    actually frequencies are stored rather than eigenvalues.  Frequencies are
+    eigenvalues with the appropriate weighting for (from the mass or from rigid bodies)
+    
+    Parameters
+    ----------
+    m : Minimum object
+    eigenvalues : array
+        the list of eigenvalues
+    eigenvectors : 2-d numpy array
+        vectors[:,i] is the eigenvector associated to the normal mode frequency freq[i].
+        the eigenvectors should be orthogonal and normalized
+    """
+    __tablename__ = "tbl_hessian_eigs"
+
+    eigenvalues = deferred(Column(PickleType))
+    eigenvectors = deferred(Column(PickleType))
+
+    _minimum_id = Column(Integer, ForeignKey('tbl_minima._id'), primary_key=True)
+    minimum = relationship("Minimum",
+                            primaryjoin="Minimum._id==HessianEigs._minimum_id",
+                            backref='hessian_eigs', uselist=False)
+    def __init__(self, m, eigenvalues, eigenvectors):
+        self.minimum = m
+        self.eigenvalues = eigenvalues
+        self.eigenvectors = np.copy(eigenvectors)
+
 
 if __name__ == "__main__":
     from pygmin.systems import LJCluster
@@ -50,7 +88,7 @@ if __name__ == "__main__":
     pot = system.get_potential()
     e, g, hess = pot.getEnergyGradientHessian(coords)
     eval, evec = get_sorted_eig(hess)
-    epair = NormalModes(m, eval, evec)
+    epair = NormalModes(m, eval, evec, nzero=6, nnegative=0)
     db.session.commit()
     print m.normal_modes[0].vectors.shape
     
