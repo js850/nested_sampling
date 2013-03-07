a4e41b3bfb7147de5daf070589dedca6c14e9302
Jacob Stevenson <js850@cam.ac.uk>
2013-03-07 14:12:25 +0000

    fix a bug in monte carolo class and add adaptive step size

--- nested_sampling.py
+++ nested_sampling.py
@@ -30,7 +30,7 @@ class MonteCarloChain(object):
         
         if accept:
             self.x = xnew
-            self.energy
+            self.energy = e
             self.naccept += 1
         
         self.nsteps += 1
@@ -43,13 +43,16 @@ class Replica(object):
 
 class NestedSampling(object):
     """the main class for implementing nested sampling"""
-    def __init__(self, system, nreplicas, mciter=100):
+    def __init__(self, system, nreplicas, takestep, mciter=100):
         self.system = system
+        self.takestep = takestep
         self.mciter=mciter
         
         self.max_energies = []
         
         self.setup_replicas(nreplicas)
+    
+    
     
     def create_replica(self):
         x = self.system.get_random_configuration()
@@ -66,17 +69,34 @@ class NestedSampling(object):
         print "min replica energy", self.replicas[0].energy
         print "max replica energy", self.replicas[-1].energy
     
+    def adjust_step_size(self, mc):
+        f = 0.8
+        target_ratio = 0.7
+        max_stepsize = 0.5
+        ratio = float(mc.naccept) / mc.nsteps
+        if ratio < target_ratio:
+            # reduce step size
+            self.takestep.stepsize *= f
+        else:
+            self.takestep.stepsize /= f
+        if self.takestep.stepsize > max_stepsize:
+            self.takestep.stepsize = max_stepsize
+    
     def sample_replica(self, Emax):
         # choose a replica randomly
         rstart = random.choice(self.replicas)
         
         # do a monte carlo iteration
-        mc = MonteCarloChain(self.system.get_potential(), rstart.x, self.system.get_takestep(), Emax)
+        mc = MonteCarloChain(self.system.get_potential(), rstart.x, self.takestep, Emax)
         for i in range(self.mciter):
             mc.step()
+        self.adjust_step_size(mc)
         
-        return Replica(mc.x, mc.energy)
-            
+        # print some data
+        print "step: %accept", float(mc.naccept) / mc.nsteps, "energy new old max", mc.energy, rstart.energy, Emax, "stepsize", self.takestep.stepsize
+        
+        
+        return Replica(mc.x, mc.energy)         
     
     def one_iteration(self):
         # pull out the replica with the largest energy
@@ -93,12 +113,29 @@ class NestedSampling(object):
         
 if __name__ == "__main__":
     from bh_sampling import LJClusterNew
+    from pygmin.takestep import RandomDisplacement
     natoms = 13
-    nreplicas = 100
+    nreplicas = 500
+    mciter = 100
     system = LJClusterNew(natoms)
     
-    ns = NestedSampling(system, nreplicas)
-    for i in range(10):
+    
+    ns = NestedSampling(system, nreplicas, RandomDisplacement(stepsize=0.5), mciter=mciter)
+    for i in range(20000):
         ns.one_iteration()
     print ns.max_energies
+    print "min replica energy", ns.replicas[0].energy
+    print "max replica energy", ns.replicas[-1].energy
+    
+    with open("max_energies", "w") as fout:
+        fout.write( "\n".join([ str(e) for e in ns.max_energies]) )
+        fout.write("\n")
+        
+    
+    if True:
+        import pylab as pl
+        e = np.array(ns.max_energies)
+        pl.plot(e)
+        pl.show()
+
     
