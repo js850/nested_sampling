bb8c81936212b9d4410f2b4dd947dcffc49e0254
Stefano Martiniani <stefano.martiniani@gmail.com>
2013-03-12 18:31:20 +0000

    added comments (still raw)

--- nested_sampling.py
+++ nested_sampling.py
@@ -5,8 +5,32 @@ import sys
 
 class MonteCarloChain(object):
     """from an initial configuration do a monte carlo chain with niter iterations
-    
-    the step will be accepted subject only to a maximum energy criterion
+        the step will be accepted subject only to a maximum energy criterion
+
+    Parameters
+    -----------
+    potential : pygmin Potentials
+        attribute of system with member function getEnergy (in essence a particular potential energy function)
+    x : object
+        are the coordinates
+    takestep : pygmin LJCLuster
+        take a random montecarlo step, imported from pygmin: takestep(x) makes a move from x
+    Emax : object
+        energy upperbound
+    energy : pygmin LJCluster
+        energy of a given configuration
+    accept_test : pygmin Acceptance Tests
+        it's an array of pointers to functions. The dereferenced functions operate a set of tests on the energy/configuration.
+    event : object
+        it's an array of pointers to functions. This is general and not compulsury e.g. can use if you want to do something with the new configuration for the guy.
+    nsteps : object
+        tot. number of steps
+    naccepts : object
+        tot. number of accepted configurations
+    xnew : object
+        new proposed configuration
+    accept : object
+        boolean, true or false if energy constraint is satisfied
     """
     def __init__(self, potential, x0, takestep, Emax, accept_tests=None, events=None):
         self.potential = potential
@@ -52,12 +76,27 @@ class MonteCarloChain(object):
 
        
 class Replica(object):
+    """ Replica is simply a pair of types coordinates (x) and energy"""
     def __init__(self, x, energy):
         self.x = x.copy()
         self.energy = float(energy)
 
 class NestedSampling(object):
-    """the main class for implementing nested sampling"""
+    """the main class for implementing nested sampling
+
+    Parameters
+    ----------
+    system : pygmin System
+        is the particular system of interest, say LJCluster
+    takestep : object
+        take a random montecarlo step, imported from pygmin: takestep(x) makes a move from x
+    mciter : object
+        number of steps in markov chain (sampling)
+    accept_test : python Acceptance Tests
+        it's an array of pointers to functions. The dereferenced functions operate a set of tests on the energy/configuration.
+    max_energies : object
+        array of stored energies (at each step the highest energy configuration is stored and replaced by a valid configuration)
+        """
     def __init__(self, system, nreplicas, takestep, mciter=100, accept_tests=None):
         self.system = system
         self.takestep = takestep
@@ -76,21 +115,32 @@ class NestedSampling(object):
     
     
     def create_replica(self):
+        """
+        creates a random configuration, evaluates its energy and creates the corresponding Replica object
+        """
         x = self.system.get_random_configuration()
         pot = self.system.get_potential()
         e = pot.getEnergy(x)
         return Replica(x, e)
-    
     def sort_replicas(self):
+        """
+        sorts the replicas in decreasing order of energy
+        """
         self.replicas.sort(key=lambda r:r.energy)
     
     def setup_replicas(self, nreplicas):
+        """
+        creates nreplicas replicas and sorts them in decreasing order of energy
+        """
         self.replicas = [self.create_replica() for i in range(nreplicas)]
         self.sort_replicas()
         print "min replica energy", self.replicas[0].energy
         print "max replica energy", self.replicas[-1].energy
     
     def adjust_step_size(self, mc):
+        """
+        when ratio naccept/nsteps < target_ratio then decrease step size by a correction factor f (new-step-size = old-step-size*f, where 0<f<1), else if naccept/nsteps > target_ratio, then increases the step size (new-step-size = old-step-size/f, where 0<f<1), although this is increased with an upper bound: max_stepsize.
+        """
         f = 0.8
         target_ratio = 0.7
         max_stepsize = 0.5
