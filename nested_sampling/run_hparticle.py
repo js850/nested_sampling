5d1bd8ca287d89189c65a316f86d967af60e2908
Stefano Martiniani <stefano.martiniani@gmail.com>
2013-06-18 14:52:08 +0100

    fixed bool parses in run hparticle

--- nested_sampling/run_hparticle.py
+++ nested_sampling/run_hparticle.py
@@ -13,7 +13,7 @@ def main():
     parser.add_argument("-K", "--nreplicas", type=int, help="number of replicas", default=300)
     parser.add_argument("-A", "--ndof", type=int, help="number of degrees of freedom", default=4)
     parser.add_argument("-P", "--nproc", type=int, help="number of processors", default=1)
-    parser.add_argument("-p", "--trivparal", type=bool, help="set whether to do trivial parallelisation, by default True",default=True)
+    parser.add_argument("--trivparal", action='store_true', help="set whether to do trivial parallelisation",default=False)
     parser.add_argument("-q", action="store_true", help="turn off verbose printing of information at every step")
     args = parser.parse_args()
 
