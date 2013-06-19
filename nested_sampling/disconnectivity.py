2fe7ee6f83a973bd7b1741724f5eae356568e51c
Stefano Martiniani <stefano.martiniani@gmail.com>
2013-06-19 15:01:12 +0100

    written script to plot disconnectivity graph

---
+++ nested_sampling/disconnectivity.py
@@ -0,0 +1,17 @@
+from pygmin.landscape import Graph
+from pygmin.utils.disconnectivity_graph import DisconnectivityGraph
+import matplotlib.pyplot as plt
+from lj_run import LJClusterNew
+import sys
+
+natoms = int(sys.argv[1])
+dbname = sys.argv[2]
+system = LJClusterNew(natoms)
+database = system.create_database(dbname)
+
+graph=Graph(database)
+dg=DisconnectivityGraph(graph.graph)
+dg.calculate()
+dg.plot()
+plt.savefig('discgraph_LJ{n}.pdf'.format(n = natoms))
+plt.show()
