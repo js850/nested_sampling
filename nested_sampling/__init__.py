e4a2c40ec5b5f538b69f1a1b2bf1d38cb584c534
Jacob Stevenson <js850@cam.ac.uk>
2013-09-04 18:25:20 +0100

    make sens.Replica accessable

--- nested_sampling/__init__.py
+++ nested_sampling/__init__.py
@@ -13,7 +13,7 @@
 
 from utils.result import Result
 from mc_walker import MonteCarloWalker
-from _nested_sampling import NestedSampling
+from _nested_sampling import NestedSampling, Replica
 from nested_sampling_runner import run_nested_sampling
 
 from models.harmonic import Harmonic
