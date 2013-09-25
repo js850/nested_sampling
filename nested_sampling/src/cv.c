2f30b5efca513772a2d1c0873ddafdea65f6a70e
Stefano Martiniani <stefano.martiniani@gmail.com>
2013-09-25 13:48:20 +0100

    added scripts for error analysis and implemented compute_dos_log and compute_dos_alpha_log, the error analysis scripts need to be updated to work with these changes, compute_cv.py works well

--- nested_sampling/src/cv.c
+++ nested_sampling/src/cv.c
@@ -4,10 +4,11 @@
 #include <float.h>
 double max_array(double* a, int N);
 double X_imp(int i, double K, double P);
-void compute_dos(double* gl, int N, double P, double K, int live);
+void compute_dos_log(double* gl, int N, double P, double K, int live);
+void compute_dos_alpha_log(double* gl, double* rn, int N, double K, int live);
 void log_weights(double* El, double* gl, double* wl, int N, double T);
 double heat_capacity(double* El, double* wl, int N, double T, double ndof, double * U, double * U2);
-void heat_capacity_loop(double* El, double* gl, double* wl, double* Cvl, double * U, double * U2, double * Tlist, int N, int nT, double ndof);
+void heat_capacity_loop(double* El, double* gl, double* wl, double* Cvl, double * Ul, double * U2l, double * Tlist, int N, int nT, double ndof);
 
 double max_array(double* a, int N)
 {
@@ -16,9 +17,9 @@ double max_array(double* a, int N)
   for (i=0; i<N; ++i)
     {
       if (a[i]>max)
-        {
-          max=a[i];
-        }
+	{
+	  max=a[i];
+	}
     }
   return max;
 }
@@ -30,17 +31,16 @@ double X_imp(int i, double K, double P)
   return X;
 }
 
-void compute_dos(double* gl, int N, double P, double K, int live)
+void compute_dos_log(double* gl, int N, double P, double K, int live)
 {
   // gl is an array of 0's of size N, K is the number of replicas
-  int i,lim;
-  double X;
-  double Xm = X_imp(0,K,P); //this is X1
-  double Xf, Xb;
-  Xb = 2-Xm; // reflecting boundary condition, this is X0
-  Xf = Xm * X_imp(1,K,P); 
-  gl[0] = 0.5 * (Xb - Xf);
-  //calculate density of states for stored energies, don't do it because of numerical instability
+  int i,j,lim;
+  double Xf;
+  double m;
+  //step i =0, m here is Xb
+  m = log(2. - X_imp(0,K,P)); // reflecting boundary condition, this is X0 
+  gl[0] = log(0.5) + m + log(1-X_imp(0,K,P)*X_imp(1,K,P));
+  
   //for(i=1;i<(N-K-1);++i) when using live replica
   if (live == 1)
     {
@@ -52,41 +52,91 @@ void compute_dos(double* gl, int N, double P, double K, int live)
     }
   for(i=1;i<lim;++i)
     {
-      //printf("Xm %E \n",Xm);
-      Xb = Xm;
-      Xm = Xf;
-      Xf = Xm * X_imp(i+1,K,P);
-      gl[i] = 0.5 * (Xb - Xf);
+      m += log(X_imp(i-1,K,P));
+      gl[i] = log(0.5) + m + log(1 - X_imp(i,K,P) * X_imp(i+1,K,P));
+      //printf("gl[%d] is %E \n",i, gl[i]);
     }
   //calculate density of states for live replica energies (if flag is on)
   if (live == 1)
     {
-      for(i=live;i<(N-1);++i)
-        {
-          Xb = Xm;
-          Xm = Xf;
-          Xf = Xm * (K-i)/(K-i+1);
-          gl[i] = 0.5 * (Xb - Xf);
-        }
+      m += log(X_imp(i,K,P));
+      j = 0;
+      for(i=lim;i<(N-1);++i)
+	{
+	  gl[i] = log(0.5) + m + log(1 - (K-j)/(K-j+1) * (K-(j+1))/(K-(j+1)+1));
+	  m += log((K-j)/(K-j+1));
+	  ++j;
+	}
+      ++i;
+      m -= log((K-(j-1))/(K-(j-1)+1));
+      Xf = -(K-j)/(K-j+1);
+      gl[N-1] = log(0.5) + m + log(1 - (K-j)/(K-j+1) * Xf);
     }
-  Xb = Xm;
-  Xm = Xf;
-  Xf = -Xm;
-  gl[N-1] =  0.5 * (Xb - Xf);  
+  else
+    {
+      ++i;
+      m += log(X_imp(i-1,K,P));
+      Xf = - X_imp(i,K,P);
+      gl[N-1] = log(0.5) + m + log(1 - X_imp(i,K,P) * Xf);
+    }
+}
+
+void compute_dos_alpha_log(double* gl, double* rn, int N, double K, int live)
+{
+  // gl is an array of 0's of size N, K is the number of replicas
+  int i,j,lim;
+  double Xf;
+  double m;
+  //step i =0, m here is Xb
+  m = log(2. - rn[0]); // reflecting boundary condition, this is X0 
+  gl[0] = log(0.5) + m + log(1-rn[0]*rn[1]);
+  
+  //for(i=1;i<(N-K-1);++i) when using live replica
+  if (live == 1)
+    {
+      lim = (N-K-1);
+    }
+  else
+    {
+      lim = (N-1);
+    }
+  for(i=1;i<lim;++i)
+    {
+      m += log(rn[i-1]);
+      gl[i] = log(0.5) + m + log(1 - (rn[i] * rn[i+1]));
+      //printf("gl[%d] is %E \n",i, gl[i]);
+    }
+  //calculate density of states for live replica energies (if flag is on)
+  if (live == 1)
+    {
+      j = 0;
+      for(i=lim;i<(N-1);++i)
+	{
+	  m += log(rn[i-1]);
+	  gl[i] = log(0.5) + m + log(1 - rn[i] * rn[i+1]);
+	  ++j;
+	}
+      ++i;
+      Xf = -rn[i];
+      gl[N-1] = log(0.5) + m + log(1 - rn[i] * Xf);
+    }
+  else
+    {
+      ++i;
+      m += log(rn[i-1]);
+      Xf = - rn[i];
+      gl[N-1] = log(0.5) + m + log(1 - rn[i] * Xf);
+    }
 }
 
 void log_weights(double* El, double* gl, double* wl, int N, double T)
 {
   int i;
   double beta = 1/T;
+  
   for(i=0;i<N;++i)
     {
-      if(gl[i]<0)
-        {
-          printf("gl[%d] is %E \n",i, gl[i]);
-          abort();
-        }
-      wl[i] = log(gl[i]) - beta * El[i];
+      wl[i] = gl[i] - beta * El[i];
     }
 }
 
@@ -102,15 +152,14 @@ double heat_capacity(double* El, double* wl, int N, double T, double ndof, doubl
   double beta = 1./T;
   double bolz = 0.;
 
-  
   for(i=0;i<N;++i)
-  {
-    bolz = exp(wl[i]);
-    Z += bolz;
-    //printf("Z %E \n",Z);
-    _U += El[i] * bolz;
-    _U2 += El[i] * El[i] * bolz;
-  }
+    {
+      bolz = exp(wl[i]);
+      Z += bolz;
+      //printf("Z %E \n",Z);
+      _U += El[i] * bolz;
+      _U2 += El[i] * El[i] * bolz;
+    }
 
   _U /= Z;
   _U2 /= Z;
@@ -122,7 +171,7 @@ double heat_capacity(double* El, double* wl, int N, double T, double ndof, doubl
 }
 
 //////////////////////////////calculate heat capacity over a set of Ts/////////////////////
-void heat_capacity_loop(double* El, double* gl, double* wl, double* Cvl, double * U, double * U2, double * Tlist, int N, int nT, double ndof)
+void heat_capacity_loop(double* El, double* gl, double* wl, double* Cvl, double * Ul, double * U2l, double * Tlist, int N, int nT, double ndof)
 {
   //Cvl is a 0's array of size N (same size as El)
   //the mean internal energy for each temperature will be returned in U
@@ -134,23 +183,23 @@ void heat_capacity_loop(double* El, double* gl, double* wl, double* Cvl, double 
   double _U2 = 0.;
   
   for(i=0;i<nT;++i)
-  {
-    T = Tlist[i];
-    log_weights(El, gl, wl, N, T);
-    wl_max = max_array(wl,N);
+    {
+      T = Tlist[i];
+      log_weights(El, gl, wl, N, T);
+      wl_max = max_array(wl,N);
     
-    //printf("wl_max %d \n",wl_max);
+      //printf("wl_max %d \n",wl_max);
     
-    for(j=0;j<N;++j)
-      {
-        wl[j] -= wl_max;
-      }
+      for(j=0;j<N;++j)
+	{
+	  wl[j] -= wl_max;
+	}
     
-    //printf("i %d\n", i);
-    Cvl[i] = heat_capacity(El, wl, N, T, ndof, &_U, &_U2);
-    //printf("    done with heat_capacity\n");
-    U[i] = _U;
-    U2[i] = _U2;
-    //printf("    done\n");
-  }
+      //printf("i %d\n", i);
+      Cvl[i] = heat_capacity(El, wl, N, T, ndof, &_U, &_U2);
+      //printf("    done with heat_capacity\n");
+      Ul[i] = _U;
+      U2l[i] = _U2;
+      //printf("    done\n");
+    }
 }
